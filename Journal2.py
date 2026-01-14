d201e3221f25672880f0eac44062382b3511b60f
isalaza6-create <isalaza6@ucsc.edu>
2026-01-14 12:08:40 -0800

    Add files via upload

---
+++ Journal2.py
@@ -0,0 +1,15 @@
+Float1=float(4.3243)
+Float2=float(2.4566)
+sum=float1+float2
+print(sum)
+print(type(sum))
+
+integer1=int(22)
+integer2=int(45)
+diff=integer1-integer2
+print(diff)
+print(type(diff))
+
+product=float1+integer2
+print(product)
+print(type(product))
