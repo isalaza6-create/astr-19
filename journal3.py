02fb15fdf9df0f0a01c22fa01d9e7bc7ba365c0a
isalaza6-create <isalaza6@ucsc.edu>
2026-01-14 12:40:52 -0800

    Add files via upload

---
+++ journal3.py
@@ -0,0 +1,20 @@
+import numpy as np 
+
+
+# def expo(x): 
+# 	return np.exp(x)
+
+
+
+def fn(x):
+	return x**3+8
+
+
+def main():
+	x=9
+	print(x)
+
+	print(fn(x))
+
+if __name__ == "__main__":
+	main()
