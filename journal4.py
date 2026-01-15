dfba33964ebdce7cd5c1a3415d148932fac506fe
isalaza6-create <isalaza6@ucsc.edu>
2026-01-14 17:18:06 -0800

    Update journal4.py

--- journal4.py
+++ journal4.py
@@ -1,17 +1,19 @@
 class dog:
 
-	def __init__(self, legs, eyes, fur, tail):
+	def __init__(self, arms, legs, eyes, fur, tail):
 
-		self.legs=int(legs)
+		self.arms=float(arms)
+		self.legs=float(legs)
 		self.eyes=int(eyes)
 		self.fur = bool(fur)
 		self.tail = bool(tail)
 
 def main():
 
-	mydog = dog(legs=4,eyes=2,fur=True,tail=True)
+	mydog = dog(arms=0.4,legs=0.6,eyes=2,fur=True,tail=True)
 
-	print("legs=4")
+	print("arms=0.4")
+	print("legs=0.6")
 	print("eyes=2")
 	print("fur=true")
 	print("tail=true")
